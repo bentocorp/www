2cfc7f0ae25c02d5ab40a659572fc00bb0eb4ef4
Dave Snyder <daverted@gmail.com>
2015-07-09 20:42:15 -0600

    patched bootstrap v3.3.4 with www ea8c3bc145

---
+++ config/deploy/prod.rb
@@ -0,0 +1,51 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
+role :app, %w{bento-prod-api1 bento-prod-api2}
+#role :web, %w{some-other-thing}
+#role :db,  %w{deploy@example.com}
+
+# Set Branch
+set :branch, 'master'
+
+#require custom config
+require './config/myconfig.rb'
+
+
+=begin
+namespace :deploy do
+
+  desc 'Get stuff ready prior to symlinking'
+  task :compile_assets do
+    on roles(:app), in: :sequence, wait: 1 do
+      execute "cp #{deploy_to}/../components/.env.php #{release_path}"
+      #execute "pwd"
+      #within release_path do
+      #  execute "cd ./build_scripts"
+      #end
+    end
+  end
+  
+  after :updated, :compile_assets
+
+end
+
+
+# Devops commands
+namespace :ops do
+
+  desc 'Copy non-git ENV specific files to servers.'
+  task :put_env_components do
+    on roles(:app), in: :sequence, wait: 1 do
+      upload! './.env.php', "#{deploy_to}/../components/.env.php"
+      execute "cp #{deploy_to}/../components/.env.php #{release_path}"
+    end
+  end
+ 
+end
+=end
+
+
---
+++ js/main.js
@@ -0,0 +1,75 @@
+
+   
+//facebook
+(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=791688527544905&version=v2.0";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+/* Facebook Conversion Code for Key Page Views - Bento Ad Account 1 */
+
+(function() {
+      var _fbq = window._fbq || (window._fbq = []);
+      if (!_fbq.loaded) {
+        var fbds = document.createElement('script');
+        fbds.async = true;
+        fbds.src = '//connect.facebook.net/en_US/fbds.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(fbds, s);
+        _fbq.loaded = true;
+      }
+    })();
+    window._fbq = window._fbq || [];
+    window._fbq.push(['track', '6026847103468', {'value':'0.00','currency':'USD'}]);
+
+// Google Analytics
+    
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-61640336-1', 'auto');
+ga('send', 'pageview');
+
+
+function setupMap(){
+	L.mapbox.accessToken = 'pk.eyJ1IjoidmluY2VudC1iZW50b25vdy1jb20iLCJhIjoiV0p2al9qNCJ9.cKufaBUS30xSk7wXxmGuDg';
+	var map = L.mapbox.map('delivery-map', 'vincent-bentonow-com.m26hh48o', {
+	  zoomControl: true
+	}).setView([37.779360, -122.412277], 13);
+	// Disable drag and zoom handlers.
+	//map.dragging.disable();
+	map.touchZoom.disable();
+	map.doubleClickZoom.disable();
+	map.scrollWheelZoom.disable();
+
+	// Disable tap handler, if present.
+	//if (map.tap) map.tap.disable();
+
+}
+
+$(document).ready(function() {
+
+	/*
+	var emailform = $('#mc-embedded-subscribe-form');
+	if(emailform.length && typeof emailform.validateForm == 'function'){
+		emailform.validateForm({
+		  	errorPlacement: function(error, element) {
+		     	error.appendTo('#mce-email-error');
+		   	},
+		   	submitHandler : function(form) {
+			    form.submit();
+			}
+		});
+	}else{
+		//console.error('no validate');
+
+	}
+	*/
+	setupMap();
+
+});

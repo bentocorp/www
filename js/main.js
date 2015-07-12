e8fee44c7270110bc869c21e512de60c93a6eabb
Dave Snyder <daverted@gmail.com>
2015-07-11 19:26:36 -0600

    disable bootstrap's default js, enable main.js

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ fjs.parentNode.insertBefore(js, fjs);
     window._fbq.push(['track', '6026847103468', {'value':'0.00','currency':'USD'}]);
 
 // Google Analytics
-    
+/* jshint ignore:start */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -34,6 +34,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-61640336-1', 'auto');
 ga('send', 'pageview');
+/* jshint ignore:end */
 
 
 function setupMap(){
@@ -71,5 +72,5 @@ $(document).ready(function() {
 	}
 	*/
 	setupMap();
-
+  console.log('hello world');
 });
